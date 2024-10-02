b0fb7ecab7334d1264b73fb0d01cd780a2997052
てつ <akasatanahamayarawawooon@gmail.com>
2024-10-03 00:44:57 +0900

    教材内容を更新しました

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [ContactController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
